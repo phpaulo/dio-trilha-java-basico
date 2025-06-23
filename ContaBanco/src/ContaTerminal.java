d6377b87adfa269b2c2ba0a6221de9a21e05da63
phpaulo <ph.paulo01@gmail.com>
2025-06-23 15:07:03 -0300

    Adicionado comentários com uma lista de tarefas para implementar este projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //TODO: Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
     }
 
     public static void Teste() {
